9e9120d20de0ea0eb4a58566ec8b21a19ac6830f
davidingles <davidingleslopez@gmail.com>
2024-02-02 11:42:30 +0100

    arreglo muy bien los Models2

--- src/components/Three.jsx
+++ src/components/Three.jsx
@@ -12,9 +12,9 @@ const Models = [
   // { title: 'CajaSeparador', url: './gltf/CajaSeparador.glb' },
   // { title: 'jamoneroMacondo2', url: './gltf/jamoneroMacondo2.glb' },
   // { title: 'jamoneroMacondo2', url: './gltf/jamoneroMacondo2.glb' },
-  { title: 'antorcha2', url: './antorcha2.glb', miEscala: 2, miPosicion: 0 },
-  { title: 'antorcha1', url: './antorcha1.glb', miEscala: .5, miPosicion: 0 },
-  { title: 'donaciones', url: './donaciones.glb', miEscala: .1, miPosicion: 0 },
+  { title: 'antorcha2', url: './antorcha2.glb', miEscala: .5, miPosicion: -0.3 },
+  { title: 'antorcha1', url: './antorcha1.glb', miEscala: .5, miPosicion: -0.3 },
+  { title: 'donaciones', url: './donaciones.glb', miEscala: .2, miPosicion: -0.3 },
 ]
 
 function Model({ url, miEscala, miPosicion }) {
